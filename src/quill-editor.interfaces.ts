fed17b9683a7d32ad34073cd5d595d729f165e8a
若海 <mail@anrip.com>
2018-11-30 01:59:31 +0800

    强化toolbar配置类型检测

--- src/quill-editor.interfaces.ts
+++ src/quill-editor.interfaces.ts
@@ -3,25 +3,22 @@ export interface CustomOption {
     whitelist: Array<any>;
 }
 
-
-export type QuillToolbarConfig = ((string | {
-    indent?: string
-    list?: string
-    direction?: string
-    header?: number | (boolean | number)[]
-    color?: string[]
-    background?: string[]
-    align?: string[]
-    script?: string
-    font?: string[]
-    size?: (boolean | string)[]
-})[])[];
-
-
-export interface QuillModules {
-    toolbar: QuillToolbarConfig;
+export interface QuillToolbar {
+    indent?: string;
+    list?: string;
+    direction?: string;
+    header?: number | (boolean | number)[];
+    color?: string[];
+    background?: string[];
+    align?: string[];
+    script?: string;
+    font?: string[];
+    size?: (boolean | string)[];
 }
 
+export interface QuillModules {
+    toolbar: ((string | QuillToolbar)[])[] | { container: (string | QuillToolbar)[] };
+}
 
 export interface QuillConfig {
     modules?: QuillModules;
